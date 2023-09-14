e072554adab3f29c8163903c9996f682bd90d11e
bresch <[brescianimathieu@gmail.com](mailto:brescianimathieu@gmail.com)>
2023-09-14 09:58:56 +0200

    simplify state var constraint

--- src/modules/ekf2/EKF/ekf.h
+++ src/modules/ekf2/EKF/ekf.h
@@ -1046,6 +1046,8 @@ private:
 	// force symmetry when the argument is true
 	void fixCovarianceErrors(bool force_symmetry);
 
+	void constrainStateVar(const IdxDof &state, float min, float max);
+
 	// constrain the ekf states
 	void constrainStates();
 
